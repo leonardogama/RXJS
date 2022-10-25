42579886302e436896cee421aba39d48394710be
leonardonantuagama <leo.gama1150@gmail.com>
2022-10-25 17:34:27 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { observable, Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +28,119 @@ import { Component } from '@angular/core';
   `,
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+
   title = 'RXJS';
+
+  minhaPromisse(nome: string) : Promise<string>{
+    return new Promise((resolve, reject) => {
+      if(nome === 'Leo'){
+        setTimeout(() => {
+          resolve('Seja bem vindo ' + nome);
+        }, 1000);
+      }
+      else{
+        reject('Ops você não é o Leo');
+      }
+    })
+  }
+
+  minhaObservable(nome:string) : Observable<string>{
+    return new Observable(subscriber => {
+      if(nome === 'Leo'){
+        subscriber.next('Olá ' + nome); //callback para subscribe
+        subscriber.next('Olá novamente ' + nome);
+        setTimeout(() => {
+          subscriber.next('resposta com atraso ' + nome);
+        }, 5000);
+        subscriber.complete();
+      }
+      else{
+        subscriber.error('Ops! Deu erro! ');
+      }
+
+      
+    });
+  }
+
+  usuarioObservable(nome:string, email:string) : Observable<Usuario>{
+    return new Observable(subscriber => {
+      if(nome === 'Admin'){
+        let usuario = new Usuario(nome, email);
+        
+        setTimeout(() => {
+          subscriber.next(usuario );
+        }, 1000);
+
+        setTimeout(() => {
+          subscriber.next(usuario );
+        }, 2000);
+
+        setTimeout(() => {
+          subscriber.next(usuario );
+        }, 3000);
+
+        setTimeout(() => {
+          subscriber.next(usuario );
+        }, 4000);
+
+        setTimeout(() => {
+          subscriber.complete();
+        }, 5000);
+
+      }
+      else{
+        subscriber.error('Ops! Deu erro! ');
+      }
+
+      
+    });
+  }
+  ngOnInit(): void {
+    //this.minhaPromisse('Leo')
+    //.then(result => console.log(result));//
+
+    /*this.minhaPromisse('Jose')
+    .then(result => console.log(result))
+    .catch(erro =>console.log(erro));*/
+
+    /*
+    this.minhaObservable('Leo')
+     .subscribe(result => console.log(result),
+                erro => console.log(erro),
+                () => console.log('Fim'));
+    */
+
+    const observer = {
+      next: valor => console.log('Next: ', valor),
+      error: erro => console.log('Erro: ', erro),
+      complete: () => console.log('Fim')
+    }
+
+    /*const obs = this.minhaObservable('Leo');
+    obs.subscribe(observer);*/
+    const obs = this.usuarioObservable('Admin','admin@admin.com');
+    const subs = obs.subscribe(observer);
+
+
+    setTimeout(() =>{
+      subs.unsubscribe();
+        console.log('Conexão fechada: ' + subs.closed) //criando flag
+    }, 3500);
+  }
+
+  escrever(texto : string){
+    console.log(texto);
+  }
+
+}
+
+export class Usuario {
+  constructor(nome: string, email: string) {
+    this.nome = nome;
+    this.email = email;
+  }
+  nome: string;
+  email: string;
 }
